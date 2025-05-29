4e1695c7efb244fd89f861e414ab6cce0f5d8a10
adhu4723 <adarshdhanwis@gmail.com>
2025-05-29 09:09:56 +0530

    updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,12 +6,12 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
 exports.signup = async (req, res) => {
-  const { email, password } = req.body;
+  const { name,email, password } = req.body;
   const existing = await User.findOne({ email });
   if (existing) return res.status(400).json({ message: "User exists" });
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({ email, password: hashedPassword });
+  const user = await User.create({name, email, password: hashedPassword });
   res.status(201).json({ message: "User created", user });
 };
 
